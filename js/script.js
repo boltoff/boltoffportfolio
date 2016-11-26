9868206b77e7a43fd050a5b4396ae5dcd4bd346d
Vladislav <flowvlad@gmail.com>
2016-11-26 12:55:52 +0200

    Bar width

--- js/script.js
+++ js/script.js
@@ -1,21 +1,34 @@
-//$("#menubutton").click(function(){
-//    $("#mobilebar").animate({'left':'-1px'}, 500);
-//    $("#rightcolumn").animate({'left':'300px'}, 500);
-//});
+        $('#exitmenubutton').on('click', function(){
+            $("#mobilebar").animate({'left':'-300px'}, 500);
+            $("#mobnav").animate({'left':'-300px'}, 500);
+            $("#rightcolumn").animate({'left':'0px'}, 500);
+            $("#topbar").animate({'left':'0px'}, 500);
+            $(this).css("display", "none");
+            $("#menubutton").css("display", "block");
+        });
 
-$('#exitmenubutton').on('click', function(){
-    $("#mobilebar").animate({'left':'-300px'}, 500);
-    $("#rightcolumn").animate({'left':'0px'}, 500);
-    $("#topbar").animate({'left':'0px'}, 500);
-    $(this).css("display", "none");
-    $("#menubutton").css("display", "block");
-});
-
-$("#menubutton").on('click', function(){
-    $("#mobilebar").animate({'left':'-1px'}, 500);
-    $("#rightcolumn").animate({'left':'300px'}, 500);
-    $("#topbar").animate({'left':'300px'}, 500);
-    $(this).css("display", "none");
-    $("#exitmenubutton").css("display", "block");
-});
-
+        $("#menubutton").on('click', function(){
+            if ($('body').width() <= 1030 && $('body').width() > 400) 
+                {
+                    $("#mobilebar").animate({'left':'-1px'}, 500);
+                    $("#mobnav").animate({'left':'-26px'}, 500);
+                    $("#rightcolumn").animate({'left':'300px'}, 500);
+                    $("#topbar").animate({'left':'300px'}, 500);
+                }
+            if ($('body').width() <= 400 && $('body').width() > 320) 
+                {
+                    $("#mobilebar").animate({'left':'-80px'}, 500);
+                    $("#mobnav").animate({'left':'-25px'}, 500);
+                    $("#rightcolumn").animate({'left':'220px'}, 500);
+                    $("#topbar").animate({'left':'220px'}, 500);
+                }
+            if ($('body').width() <= 320)
+                {
+                    $("#mobilebar").animate({'left':'-120px'}, 500);
+                    $("#mobnav").animate({'left':'-25px'}, 500);
+                    $("#rightcolumn").animate({'left':'180px'}, 500);
+                    $("#topbar").animate({'left':'180px'}, 500); 
+                }
+            $(this).css("display", "none");
+            $("#exitmenubutton").css("display", "block");
+        });
