4890e2e907e9182bddfa74f23c98ddb284b6fd07
Vladislav <flowvlad@gmail.com>
2016-11-26 16:07:20 +0200

    changes in mobilebar

--- js/script.js
+++ js/script.js
@@ -1,10 +1,11 @@
         $('#exitmenubutton').on('click', function(){
             $("#mobilebar").animate({'left':'-300px'}, 500);
             $("#mobnav").animate({'left':'-300px'}, 500);
+            $("#mobsoclinks").animate({'left':'-305px'}, 500);
             $("#rightcolumn").animate({'left':'0px'}, 500);
             $("#topbar").animate({'left':'0px'}, 500);
             $(this).css("display", "none");
-            $("#menubutton").css("display", "block");
+            $("#menubutton").css("display", "inline-table");
         });
 
         $("#menubutton").on('click', function(){
@@ -12,23 +13,28 @@
                 {
                     $("#mobilebar").animate({'left':'-1px'}, 500);
                     $("#mobnav").animate({'left':'-26px'}, 500);
+                    $("#mobsoclinks").animate({'left':'-26px'}, 500);
                     $("#rightcolumn").animate({'left':'300px'}, 500);
                     $("#topbar").animate({'left':'300px'}, 500);
                 }
             if ($('body').width() <= 400 && $('body').width() > 320) 
                 {
                     $("#mobilebar").animate({'left':'-80px'}, 500);
-                    $("#mobnav").animate({'left':'-25px'}, 500);
+                    $("#mobnav").animate({'left':'-26px'}, 500);
+                    $("#mobsoclinks").animate({'left':'-26px'}, 500);
                     $("#rightcolumn").animate({'left':'220px'}, 500);
                     $("#topbar").animate({'left':'220px'}, 500);
+                    $("#mobnav ul").css("margin-left", "30px");
                 }
             if ($('body').width() <= 320)
                 {
                     $("#mobilebar").animate({'left':'-120px'}, 500);
-                    $("#mobnav").animate({'left':'-25px'}, 500);
+                    $("#mobnav").animate({'left':'-26px'}, 500);
+                    $("#mobsoclinks").animate({'left':'-26px'}, 500);
                     $("#rightcolumn").animate({'left':'180px'}, 500);
-                    $("#topbar").animate({'left':'180px'}, 500); 
+                    $("#topbar").animate({'left':'180px'}, 500);
+                    $("#mobnav ul").css("margin-left", "8px");
                 }
             $(this).css("display", "none");
-            $("#exitmenubutton").css("display", "block");
+            $("#exitmenubutton").css("display", "inline-table");
         });
